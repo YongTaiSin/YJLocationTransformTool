8908afb142d74ff723c98c1bf1d8bdef54330316
Mr.Yang <971175049@qq.com>
2016-07-09 15:31:51 +0800

    init

---
+++ YJLocationTransform-master/YJLocationTransform-master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJLocationTransform-master
+//
+//  Created by Mac on 16/7/9.
+//  Copyright © 2016年 MIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YJLocationTransform-master/YJLocationTransform-master/YJLocationTransform/YJLocationTransform.h
@@ -0,0 +1,38 @@
+//
+//  YJLocationTransform.h
+//  OShopping
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 MacauIT. All rights reserved.
+//地图坐标转换工具
+
+#import <Foundation/Foundation.h>
+
+@interface YJLocationTransform : NSObject
+@property (nonatomic, assign) double latitude;
+@property (nonatomic, assign) double longitude;
+
+- (id)initWithLatitude:(double)latitude andLongitude:(double)longitude;
+
+/*
+ 坐标系：
+ WGS-84：是国际标准，GPS坐标（Google Earth使用、或者GPS模块）
+ GCJ-02：中国坐标偏移标准，Google Map、高德、腾讯使用
+ BD-09 ：百度坐标偏移标准，Baidu Map使用
+ */
+
+#pragma mark - 从GPS坐标转化到高德坐标
+- (id)transformFromGPSToGD;
+
+#pragma mark - 从高德坐标转化到百度坐标
+- (id)transformFromGDToBD;
+
+#pragma mark - 从百度坐标到高德坐标
+- (id)transformFromBDToGD;
+
+#pragma mark - 从高德坐标到GPS坐标
+- (id)transformFromGDToGPS;
+
+#pragma mark - 从百度坐标到GPS坐标
+- (id)transformFromBDToGPS;
+@end
